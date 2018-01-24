f89c113468e299d698e5b101b64783ceac8a83fc
Peter Nycander <peter.nycander@gmail.com>
2018-01-24 18:46:26 +0100

    use env port

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const port = 3050;
+const port = process.env.PORT || 3050;
 
 app.use(
   bodyParser.json({
